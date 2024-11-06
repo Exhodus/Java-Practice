8c099283228a8cdf07a8cbc18499979b142762e8
Exhodus <51028457+Exhodus@users.noreply.github.com>
2024-11-06 15:33:28 +0100

    JoanBrossa TODO

--- src/Joel/Arrays/LaGrossa.java
+++ src/Joel/Arrays/LaGrossa.java
@@ -1,6 +1,6 @@
 package Joel.Arrays;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class LaGrossa {
     public static void main(String[] args) {
@@ -8,7 +8,32 @@ public class LaGrossa {
 
         int casos = scan.nextInt();
         int calers;
-        scan.nextLine();
+        int entrades;
+        int cont = 0;
+        List<Integer> listCalers = new ArrayList<Integer>();
 
+        for(int j = 0; j < casos; j++) {
+            calers = scan.nextInt();
+            entrades = scan.nextInt();
+            while (entrades != 0) {
+                listCalers.add(entrades);
+                entrades = scan.nextInt();
+            }
+
+            Collections.sort(listCalers);
+
+            for (int i = 0; i < listCalers.size(); i++) {
+                if (calers > 0) {
+                    calers -= listCalers.get(i);
+                    if (calers >= 0) {
+                        cont++;
+                    }
+                }
+            }
+
+            System.out.println(cont);
+            cont = 0;
+            listCalers.clear();
+        }
     }
 }
